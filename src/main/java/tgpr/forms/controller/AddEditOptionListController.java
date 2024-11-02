1086bd15af947158fae95c27d3c5333bb7a2e20b
Rasha Bouras <1804rabouras@student.epfc.eu>
2024-11-02 14:37:14 +0100

    feat(add_edit_option_list): ajout d'attributs (panel et boutons) et constructeur, incomplets

--- src/main/java/tgpr/forms/controller/AddEditOptionListController.java
+++ src/main/java/tgpr/forms/controller/AddEditOptionListController.java
@@ -1,7 +1,19 @@
 package tgpr.forms.controller;
 
+import tgpr.forms.view.ViewFormsView;
+import tgpr.framework.Controller;
 
 public class AddEditOptionListController {
 
 }
 
+/*
+public class AddEditOptionListController extends Controller<AddEditOptionListView> {
+
+    public AddEditOptionListView getView() {
+        return new AddEditOptionListView(this);
+    }
+
+}
+
+ */
--- src/main/java/tgpr/forms/view/AddEditOptionListView.java
+++ src/main/java/tgpr/forms/view/AddEditOptionListView.java
@@ -1,5 +1,23 @@
 package tgpr.forms.view;
 
-public class AddEditOptionListView {
+import com.googlecode.lanterna.gui2.Button;
+import com.googlecode.lanterna.gui2.Panel;
+import tgpr.forms.controller.ViewEditInstanceController;
 
+public class AddEditOptionListView {
+    //private final AddEditOptionListController controller;
+    private final Panel panelForOptionListUpdate = new Panel();
+    private Button reorder;
+    private Button delete;
+    private Button save;
+    private Button close;
+    private Button add;
+
+/*
+    public AddEditOptionListView(AddEditOptionListController controller) {
+        this.controller = controller;
+
+    }
+
+ */
 }
