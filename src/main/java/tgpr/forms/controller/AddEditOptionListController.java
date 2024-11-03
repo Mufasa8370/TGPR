45cc7e9ef093b80d0a3eca9b2a9065395025c722
Rasha Bouras <1804rabouras@student.epfc.eu>
2024-11-03 20:14:10 +0100

    feat(add_edit_option_list):ajout d'attributs et de méthodes dans la partie controller, incomplets

--- src/main/java/tgpr/forms/controller/AddEditOptionListController.java
+++ src/main/java/tgpr/forms/controller/AddEditOptionListController.java
@@ -1,19 +1,43 @@
 package tgpr.forms.controller;
 
-import tgpr.forms.view.ViewFormsView;
+import tgpr.forms.model.OptionList;
+import tgpr.forms.model.OptionValue;
+import tgpr.forms.view.AddEditOptionListView;
+
 import tgpr.framework.Controller;
 
-public class AddEditOptionListController {
 
-}
-
-/*
 public class AddEditOptionListController extends Controller<AddEditOptionListView> {
+    private AddEditOptionListView view;
+    private OptionList optionList;
+    /*
+    public Member save() {
+        var controller = new EditMemberController(member);
+        navigateTo(controller);
+        return controller.getMember();
+    }
+
+     */
+    /*
+    public void add(OptionValue value) {
+        optionList.addValue(value);
+    }
+
+     */
+    public void add(){}
+
+    public void reorder(){}
+    public void delete(){}
+    public void save(){}
+    public void close(){}
+
+
 
     public AddEditOptionListView getView() {
-        return new AddEditOptionListView(this);
+        //return new AddEditOptionListView(this);
+        return view;
     }
 
 }
 
- */
+
